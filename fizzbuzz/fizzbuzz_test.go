2a0ef0c1476758ac6aae27bb18d3942752824ac6
yuzujoe <yuzun225@gmail.com>
2020-03-08 13:46:31 +0900

    add test case

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -10,7 +10,17 @@ func TestConvert(t *testing.T) {
 		{n: 1, want: "1"},
 		{n: 2, want: "2"},
 		{n: 3, want: "Fizz"},
+		{n: 4, want: "4"},
 		{n: 5, want: "Buzz"},
+		{n: 6, want: "Fizz"},
+		{n: 7, want: "7"},
+		{n: 8, want: "8"},
+		{n: 9, want: "Fizz"},
+		{n: 10, want: "Buzz"},
+		{n: 11, want: "11"},
+		{n: 12, want: "Fizz"},
+		{n: 13, want: "13"},
+		{n: 14, want: "14"},
 		{n: 15, want: "FizzBuzz"},
 	}
 
